a4de9e56c48c50eb4326bae86babfe9e9f4f7f2d
John Doe <hroch.spb@gmail.com>
2017-06-14 22:23:03 +0200

    removed ide specific file

---
+++ src/main/java/com/alchesoft/training/jboss/TheatreBox.java
@@ -0,0 +1,13 @@
+package com.alchesoft.training.jboss;
+
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+
+/**
+ * Created by alex on 13.06.17.
+ */
+@Singleton
+@Startup
+public class TheatreBox {
+
+}
