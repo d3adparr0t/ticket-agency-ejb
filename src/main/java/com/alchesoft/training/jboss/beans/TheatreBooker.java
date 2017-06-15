42c1d7f56083184a7de7f8bd23b0b25ac6cf89a3
John Doe <hroch.spb@gmail.com>
2017-06-15 22:53:26 +0200

    - removed constants from remote interfaces

--- src/main/java/com/alchesoft/training/jboss/beans/TheatreBooker.java
+++ src/main/java/com/alchesoft/training/jboss/beans/TheatreBooker.java
@@ -1,5 +1,6 @@
 package com.alchesoft.training.jboss.beans;
 
 public interface TheatreBooker {
+
     String bookSeat(int seatId) throws Exception;
 }
--- src/main/java/com/alchesoft/training/jboss/beans/TheatreInfo.java
+++ src/main/java/com/alchesoft/training/jboss/beans/TheatreInfo.java
@@ -1,5 +1,6 @@
 package com.alchesoft.training.jboss.beans;
 
 public interface TheatreInfo {
+
     String printSeats();
 }
